e47128187ef84d47ec2214d639397621a6effee0
Thilo Stier <thilodaniel.stier@uni-goettingen.de>
2024-09-26 13:27:54 +0200

    add del statements in __init__.py to clean up namespace

--- src/atmosphere_bgs/__init__.py
+++ src/atmosphere_bgs/__init__.py
@@ -1,4 +1,8 @@
 #from atmosphere_bgs.main import *
 from atmosphere_bgs.ot_solver import OTSolver
 from atmosphere_bgs.prepare_data import DataLoader
-from atmosphere_bgs.plotting import *
+from atmosphere_bgs.plotting import plot_lag_tess, plot_pressure_surface, get_u
+
+del ot_solver
+del prepare_data
+del plotting
