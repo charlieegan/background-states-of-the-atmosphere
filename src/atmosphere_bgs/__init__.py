8175bb1e53faff8e50f79569536bfb1cd227ca17
Thilo Stier <thilodaniel.stier@uni-goettingen.de>
2024-11-29 14:09:27 +0100

    expose LaguerreDiagram in main module

--- src/atmosphere_bgs/__init__.py
+++ src/atmosphere_bgs/__init__.py
@@ -3,6 +3,8 @@ from atmosphere_bgs.ot_solver import OTSolver
 from atmosphere_bgs.prepare_data import DataLoader
 from atmosphere_bgs.plotting import plot_lag_tess, plot_pressure_surface, get_u
 
+from _atmosphere_bgs import LaguerreDiagram
+
 del ot_solver
 del prepare_data
 del plotting
