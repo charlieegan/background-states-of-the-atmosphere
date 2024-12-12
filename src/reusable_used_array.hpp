e6dad6598abc78eff4447f225ab3cff083d9d01d
Thilo Stier <thilodaniel.stier@stud.uni-goettingen.de>
2024-12-12 13:32:00 +0100

    add doc for reusable_used_array

--- src/reusable_used_array.hpp
+++ src/reusable_used_array.hpp
@@ -7,19 +7,23 @@
 #include <sstream>
 #include <concepts>
 
-// simple reusable used array that can mark, check, reset in amortized O(1)
-// a smaller type I will lead to smaller memory usage but more required hard resets (reset() will be O(n) every maxval(I) executions)
-// note that memory from this is only freed when the object is deleted
+/*! simple reusable used array that can mark, check, reset in amortized O(1)
+ * a smaller type I will lead to smaller memory usage but more required hard resets (reset() will be O(n) every maxval(I) executions)
+ * note that memory from this is only freed when the object is deleted
+ */
 template <std::signed_integral I = int>
 class reusable_used_array
 {
 public:
-  std::vector<I> a;
-  I ctr;
+  std::vector<I> a; //!< vector to store last-marked-times in
+  I ctr; // current time (if a[i] == ctr, i is marked used)
 
+  /*! Construct with initial size (default 0). Note that size does not influence behavior, only performance.
+   * \param init_size how much space to initially reserve
+   */
   reusable_used_array(const int &init_size = 0) : a(init_size, 0), ctr(1) {}
 
-  // reset array (i.e. mark everything unused)
+  /*! Reset array (i.e. mark everything unused). */
   void reset() {
     ctr++;
     if (ctr < 0) {
@@ -28,21 +32,25 @@ public:
     }
   }
 
-  // check whether element i is used
+  /*! Check whether given elementis used.
+   * \param i element to check for
+   */
   bool check(const int &i) const {
     if (i < a.size())
       return a[i] == ctr;
     return false;
   }
 
-  // mark element i as used
+  /*! Mark given element as used.
+   * \param i element to mark
+   */
   void mark(const int &i) {
     while (a.size() < i + 1)
       a.push_back(0);
     a[i] = ctr;
   }
 
-
+  /*! Output as binary representation. */
   friend std::ostream& operator<<(std::ostream &os, const reusable_used_array &a) {
     os << "used[";
     for (int i = 0; i < a.a.size(); ++i)
@@ -50,14 +58,13 @@ public:
     os << "]";
     return os;
   }
-  
+
+  /*! Generate and return binary string representation. */
   std::string repr() const {
     std::stringstream ss;
     ss << this;
     return ss.str();
   }
-
 };
-
 
 #endif
