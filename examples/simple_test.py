41942ac45f6a5685a98d09037e1f72582921d984
Charlie Egan <30265432+charlieegan@users.noreply.github.com>
2026-03-06 12:57:06 +0000

    Added simple test case

---
+++ examples/simple_test.py
@@ -0,0 +1,48 @@
+"""
+Created on Thu Mar  5 20:44:28 2026
+
+Simple test case for OT-BGS solver using LC1 data and interpolation of masses
+using a Gaussian grid in latitude. Adjust the integer parameter 'skip' to 
+change resolution of the target measure. Bigger skip means lower resolution.
+Zero skip means full resolution.
+"""
+
+import numpy as np
+import atmosphere_bgs
+
+data_type = 'lc1low'
+date = '200101'
+dtime = '0000'
+
+filepath = '../data/'
+filename = 'bs_'+data_type+date+dtime
+
+skip = 2
+input_data = atmosphere_bgs.DataLoader(filepath+filename,load_all=True,skip=skip)
+solv = atmosphere_bgs.OTSolver(input_data)
+solv.get_bgs(verbose=True)
+ld = solv.ld
+solv.ld.sim.pmax=140000
+
+# ld = atmosphere_bgs.LaguerreDiagram(input_data.y,psi,solv.pp,solv.sp)
+# ld.sim.pmax = 140000
+
+# print(np.min(ld.areas))
+
+# plot resulting Laguerre diagrams coloured by zonal angular momentum and potential temperature
+th = ld.ys[:,1].copy()
+idx_top = th == np.max(th)
+th[idx_top] = np.nan
+
+z = ld.ys[:,0].copy()
+z[idx_top] = np.nan
+
+_, ax = atmosphere_bgs.plot_lag_tess(ld,val=z,invert_yaxis=True)
+ax.set_title('Potential temperature')
+ax.set_xlabel('s')
+ax.set_ylabel('p')
+
+_, ax = atmosphere_bgs.plot_lag_tess(ld,val=th,invert_yaxis=True)
+ax.set_title('Zonal angular momentum')
+ax.set_xlabel('s')
+ax.set_ylabel('p')
