c2030183d29825d74e0d9aa3f6063006cc8b3b33
Charlie Egan <30265432+charlieegan@users.noreply.github.com>
2026-04-20 18:11:16 +0100

    update example file to be consistent with other updates

--- examples/simple_test.py
+++ examples/simple_test.py
@@ -10,16 +10,20 @@ Zero skip means full resolution.
 import numpy as np
 import atmosphere_bgs
 
+# Load input data
 data_type = 'lc1low'
 date = '200101'
 dtime = '0000'
 
 filepath = '../data/'
 filename = 'bs_'+data_type+date+dtime
+p00 = 1e+5
+pmin = 10
+input_data = atmosphere_bgs.DataLoader(filepath+filename,p00=p00,load_all=True,pmin=pmin)
 
-skip = 0
-input_data = atmosphere_bgs.DataLoader(filepath+filename,p00=1e+5,load_all=True,skip=skip)
+# solve ot problem
 solv = atmosphere_bgs.OTSolver(input_data)
+solv.ot_tol = 1e-6
 solv.get_bgs(verbose=True)
 ld = solv.ld
 solv.ld.sim.pmax=140000
@@ -39,5 +43,12 @@ ax.set_ylabel('p')
 
 _, ax = atmosphere_bgs.plot_lag_tess(ld,val=th,invert_yaxis=True)
 ax.set_title('Zonal angular momentum')
+ax.set_xlabel('s')
+ax.set_ylabel('p')
+
+u = atmosphere_bgs.get_u(ld)
+u[idx_top] = np.nan
+_, ax = atmosphere_bgs.plot_lag_tess(ld,val=u,invert_yaxis=True)
+ax.set_title('Zonal wind')
 ax.set_xlabel('s')
 ax.set_ylabel('p')
