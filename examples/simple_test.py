a01dce5881a9433460750a3e26cfd814720d7e7e
Charlie Egan <30265432+charlieegan@users.noreply.github.com>
2026-05-06 11:02:44 +0100

    update simple test

--- examples/simple_test.py
+++ examples/simple_test.py
@@ -23,7 +23,7 @@ input_data = atmosphere_bgs.DataLoader(filepath+filename,p00=p00,load_all=True,p
 
 # solve ot problem
 solv = atmosphere_bgs.OTSolver(input_data)
-solv.ot_tol = 1e-6
+solv.ot_tol = 1e-4
 solv.get_bgs(verbose=True)
 ld = solv.ld
 solv.ld.sim.pmax=140000
@@ -52,3 +52,18 @@ _, ax = atmosphere_bgs.plot_lag_tess(ld,val=u,invert_yaxis=True)
 ax.set_title('Zonal wind')
 ax.set_xlabel('s')
 ax.set_ylabel('p')
+
+# rasterize theta
+H = 6500
+
+s2lat = lambda s : np.rad2deg(np.arcsin(s))
+p2h = lambda p : -H*np.log(p/solv.pp.p00)
+
+lat2s = lambda lat : np.sin(np.deg2rad(lat))
+h2p = lambda h : solv.pp.p00*np.exp(-h/H)
+
+transform = lambda x : [s2lat(x[0]), p2h(x[1])]
+rast = ld.get_rasterizer(transform)
+
+res = [1000,1000]
+rv = rast.rasterize(solv.ys[:,1],res,calc_inverse=True)
