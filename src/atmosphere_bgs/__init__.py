a6698328a6b3d14ee9954445bb845689fb5e92b2
Charlie Egan <30265432+charlieegan@users.noreply.github.com>
2026-07-02 12:19:17 +0100

    Update __init__.py to include post processor class

--- src/atmosphere_bgs/__init__.py
+++ src/atmosphere_bgs/__init__.py
@@ -1,9 +1,11 @@
 from atmosphere_bgs.ot_solver import OTSolver
 from atmosphere_bgs.prepare_data import DataLoader
 from atmosphere_bgs.plotting import plot_lag_tess, plot_pressure_surface, get_u
+from atmosphere_bgs.post_processor import PostProcessor
 
 from _atmosphere_bgs import LaguerreDiagram
 
 del ot_solver
 del prepare_data
 del plotting
+del post_processor
