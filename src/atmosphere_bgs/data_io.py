38db29b908ddcfc631be0aee28a5490ac77feb7a
Charlie Egan <30265432+charlieegan@users.noreply.github.com>
2026-08-28 17:51:36 +0100

    add functions to rasterize sdot solution and save to netCDF

--- src/atmosphere_bgs/data_io.py
+++ src/atmosphere_bgs/data_io.py
@@ -3,7 +3,7 @@ import xarray as xr
 import os
 import _atmosphere_bgs
 
-def save_bgs_to_netCDF(data_dict,experiment_type='unspecified',file_path=None):
+def save_bgs_to_netCDF(data_dict,experiment_type=None,file_path=None):
     """Creates an xarray.Dataset for the background state.
 
     All numpy data arrays are passed as explicit inputs.
@@ -11,6 +11,9 @@ def save_bgs_to_netCDF(data_dict,experiment_type='unspecified',file_path=None):
     file_path : str
         Optional path to save the dataset as a NetCDF file. If None, the dataset is returned without saving.
     """
+    if experiment_type is None:
+        experiment_type = 'unspecified'
+        
     # Helper function to fall back to zeros if an input array is missing
     def _get_data(arr, shape=0):
         return np.nan*np.zeros(shape) if arr is None else arr
@@ -246,3 +249,97 @@ def reconstruct_laguerre_diagram(filepath):
     #ld_epv = ds['ld_ertel_potential_vorticity']
     
     return ld #, ld_epv
+
+
+def get_u(Z,s,pp):
+    '''Compute zonal wind for zonal angular momentum Z and sin-of-lat s'''
+    Omega = pp.Omega
+    a = pp.a
+    return Z/a/np.sqrt(1-s**2) - Omega*a*np.sqrt(1-s**2)
+
+def save_rasterized_sdot_bgs_to_netCDF(input_path,experiment_type=None,output_path=None,res=[256,256]):
+    '''Function to rasterize the semi-discrete optimal transport solution (before smoothing)
+    and save the rasterized (i.e. gridded) solution in netCDF format. Grid is regular rectangular
+    grtid in coordinates latitude and pseudo-height (-H*log(p/p00))
+    
+    input_path: string; path of input data containing generators and parameters of Laguerre tessellation
+    experiment_type: string; name for experiment
+    output_path: string; path on which to save output
+    res: list; length 2 list specifying resolution of rasterized result 
+    '''
+    # reconstruct the Laguerre tessellation from the generators and paramters specified by the input path
+    ld = reconstruct_laguerre_diagram(input_path)
+    
+    # define relevant latitude levels and pressure levels using pseudo-height coordinate
+    H = 6500
+    p00 = ld.phys.p00
+    
+    s2lat = lambda s : np.rad2deg(np.arcsin(s))
+    p2h = lambda p : -H*np.log(p/p00)
+    lat2s = lambda lat : np.sin(np.deg2rad(lat))
+    h2p = lambda h : p00*np.exp(-h/H)
+
+    transform = lambda x : [s2lat(x[0]), p2h(x[1])]
+    rast = ld.get_rasterizer(transform)
+            
+    smin = ld.sim.smin; smax = ld.sim.smax
+    pmin = ld.sim.pmin; pmax = ld.sim.pmax
+    lg = np.linspace(s2lat(smin),s2lat(smax),res[0]+1)
+    hg = np.linspace(p2h(pmin),p2h(pmax),res[1]+1)
+    
+    latitude_levels = lg[:-1] + np.diff(lg)/2 # midpoints
+    pressure_levels = h2p(hg[:-1] + np.diff(hg)/2) # midpoints
+    
+    # get rasterised zonal angular momentum and potential temperature
+    def rasterize_centroidal_values(val):
+        rv = rast.rasterize(val, res).copy()
+        fill = rast.fill
+        rv = np.where(fill > 0.5, np.divide(rv, fill, where=(fill > 0.5)), np.nan)
+        return rv
+
+    zonal_angular_momentum = np.flip(rasterize_centroidal_values(ld.ys[:,0]),axis=1)
+    potential_temperature = np.flip(rasterize_centroidal_values(ld.ys[:,1]),axis=1)
+
+    # get rasterised zonal wind from zonal angular momentum
+    lg, _ = np.meshgrid(latitude_levels,np.flip(p2h(pressure_levels)))
+    sg = lat2s(lg).T
+    zonal_wind = get_u(zonal_angular_momentum,sg,ld.phys)
+    #ertel_potential_vorticity = rast.rast(ld_epv,res)             ##### need cellwise epv saved to netCDF file along with seeds and weights
+
+    # extract surface pressure and interpolate onto latitude levels
+    n = len(ld.ys)
+    edges = [e for e in ld.edglist if e.pj >= n+6]
+    edge_pts, edge_idx = np.unique(np.vstack([e.ls.x for e in edges]),axis=0,return_index=True)
+    idx = np.argsort(edge_pts[:,0])
+    s_surf, p_surf = edge_pts[idx].T # Laguerre cell surface pressure
+    lat_surf = s2lat(s_surf)
+    surface_pressure = np.interp(latitude_levels,lat_surf,p_surf)
+    
+    # get surface potential temperature and zonal wind and evaluate on grid
+    surf_cell_idx = np.hstack([np.ones(len(e.ls.x),dtype=int)*(e.pi-6) for e in edges])[edge_idx][idx]
+    zam_surf, th_surf = ld.ys[surf_cell_idx].T
+    surface_zonal_angular_momentum = np.interp(latitude_levels,lat_surf,zam_surf)
+    surface_potential_temperature = np.interp(latitude_levels,lat_surf,th_surf)
+    surface_zonal_wind = get_u(surface_zonal_angular_momentum,lat2s(latitude_levels),ld.phys)
+
+    ##### need pv values on cells to be defined or need to do interpolation of pv as function of circulation so need input data...
+    ##### probably best to save values of pv on cells as obtained by interpolation of pv from circulation at point of solution
+
+    data_dict = {
+        'latitude_levels' : latitude_levels,
+        'pressure_levels' : pressure_levels,
+        'zonal_angular_momentum' : zonal_angular_momentum,
+        'potential_temperature' : potential_temperature,
+        'zonal_wind' : zonal_wind,
+        #'ertel_potential_vorticity' : ertel_potential_vorticity,
+        'surface_pressure' : surface_pressure,
+        'surface_potential_temperature' : surface_potential_temperature,
+        'surface_zonal_wind' : surface_zonal_wind,
+        'ld_duals' : ld.duals,
+        'ld_seeds' : ld.ys,
+        'ld_slims' : np.array([ld.sim.smin,ld.sim.smax]),
+        'ld_plims' : np.array([ld.sim.pmin,ld.phys.p00])
+        }
+    
+    ds = save_bgs_to_netCDF(data_dict,experiment_type=experiment_type,file_path=output_path)
+    return ds
